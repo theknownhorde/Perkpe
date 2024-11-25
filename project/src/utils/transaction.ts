b2d12e41dc10813a74a61ac067b71a92a6ea31af
Yogesh Tanwar <108342131+theknownhorde@users.noreply.github.com>
2024-11-25 23:30:24 +0530

    Delete project directory

--- project/src/utils/transaction.ts
+++
@@ -1,7 +0,0 @@
-export function generateTransactionHash(): string {
-  const randomBytes = new Uint8Array(32);
-  crypto.getRandomValues(randomBytes);
-  return '0x' + Array.from(randomBytes)
-    .map(b => b.toString(16).padStart(2, '0'))
-    .join('');
-}
