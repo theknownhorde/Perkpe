8e2266a55058436180a6888f4f4696a8dbe2e7e7
Yogesh Tanwar <thelearner981749@gmail.com>
2024-12-02 11:22:53 +0530

    Testing

--- backend/src/app.js
+++ backend/src/app.js
@@ -42,7 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Start the server
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 5003;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
